366aaa33a5647ec29eac623cc8421906ddc0163d
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2024-01-25 12:08:10 +0800

    泛型-完成

---
+++ src/objectAccessor.ts
@@ -0,0 +1,29 @@
+/**
+ * 任務：實作一個函式 `createObjectAccessor`，該函式接收一個物件，並回傳一個新的物件，該物件有兩個方法：get 和 set。
+ * get 方法用於獲取原物件的屬性值，set 方法用於設定原物件的屬性值。
+ *
+ * 範例：
+ * const obj = { name: 'John', age: 30 };
+ * const accessor = createObjectAccessor(obj);
+ * accessor.get('name') 應該回傳 'John'
+ * accessor.set('age', 31);
+ * accessor.get('age') 應該回傳 31
+ *
+ * @param obj - 一個物件
+ * @returns - 回傳一個物件，該物件有 get 和 set 兩個方法
+ */
+
+export function createObjectAccessor<T>(obj: T) {
+    
+    // 定義get函式，用於獲取元物件的屬性值
+    function get <K extends keyof T> (keyName: K ): T[K] {
+        return obj[keyName];
+    }
+
+    // 定義set函式，用於設定原物件的屬性值
+    function set <K extends keyof T> (keyName: K, value: T[K]): void {
+        obj[keyName] = value;
+    }
+
+    return { get , set };
+}
